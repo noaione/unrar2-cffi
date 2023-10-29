0cf252f3b151cac91604c5afdc73663491d96bee
noaione <noaione0809@gmail.com>
2023-10-29 14:09:28 +0700

    Test build on modern python

--- unrar/cffi/rarfile.py
+++ unrar/cffi/rarfile.py
@@ -1,31 +1,41 @@
+from __future__ import annotations
+
 import io
 from collections import OrderedDict
-from .unrarlib import RarArchive, BadRarFile, FLAGS_RHDF_DIRECTORY
+from typing import TYPE_CHECKING
+
+from .unrarlib import FLAGS_RHDF_DIRECTORY, BadRarFile, RarArchive
+
+if TYPE_CHECKING:
+    from os import PathLike
+
 
 class RarFileError(Exception):
     pass
 
+
 def is_rarfile(filename):
     """Return true if file is a valid RAR file."""
     try:
-        with RarArchive.open_for_metadata(filename):        
+        with RarArchive.open_for_metadata(filename):
             return True
-    except:
+    except Exception:
         return False
 
-class RarFile(object):
-    def __init__(self, filename):
+
+class RarFile:
+    def __init__(self, filename: PathLike):
         """Load RAR archive file with mode read only "r"."""
-        self.comment = b''
-        self.filename = filename
+        self.comment = b""
+        self.filename = str(filename)
         self.infos = OrderedDict()
-        
+
         try:
-            with RarArchive.open_for_processing(filename) as rar:            
+            with RarArchive.open_for_processing(filename) as rar:
                 self.comment = rar.comment.encode()
                 for header in rar.iterate_headers():
                     self.infos[header.FileNameW] = RarInfo(header)
-                    header.skip()             
+                    header.skip()
         except BadRarFile as err:
             raise RarFileError("Error opening rar: {0}".format(err))
 
@@ -40,12 +50,14 @@ class RarFile(object):
 
     def printdir(self):
         raise NotImplementedError()
-    
+
     def read(self, member):
         return self.open(member).read()
 
     def open(self, file_or_info):
-        member = file_or_info.filename if isinstance(file_or_info, RarInfo) else file_or_info
+        member = (
+            file_or_info.filename if isinstance(file_or_info, RarInfo) else file_or_info
+        )
         with RarArchive.open_for_processing(self.filename) as rar:
             for header in rar.iterate_headers():
                 if header.FileNameW == member:
@@ -53,7 +65,9 @@ class RarFile(object):
                     header.test(callback)
                     return callback.bytes_io
                 header.skip()
-        raise ValueError("Cannot open member file %s in rar %s" % (member, self.filename))
+        raise ValueError(
+            "Cannot open member file %s in rar %s" % (member, self.filename)
+        )
 
     def testrar(self):
         with RarArchive.open_for_processing(self.filename) as rar:
@@ -63,18 +77,22 @@ class RarFile(object):
                 except BadRarFile:
                     return header.FileNameW
 
+
 class InMemoryCollector:
     def __init__(self):
-        self._data = b''
-    def __call__(self, chunk):              
+        self._data = b""
+
+    def __call__(self, chunk):
         self._data += chunk
 
     @property
     def bytes_io(self):
-        return io.BytesIO(self._data)    
+        return io.BytesIO(self._data)
+
 
 class RarInfo(object):
     """Class with attributes describing each member in the RAR archive."""
+
     def __init__(self, header):
         """Initialize a RarInfo object with a member header data."""
         self.filename = header.FileNameW
@@ -90,15 +108,16 @@ class RarInfo(object):
     def is_dir(self):
         return self.flag_bits & FLAGS_RHDF_DIRECTORY
 
+
 # see https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-dosdatetimetofiletime
 def dostime_to_timetuple(dostime):
     """Convert a RAR archive member DOS time to a Python time tuple."""
-    date = dostime >> 16 & 0xffff
-    time = dostime & 0xffff   
-    day = date & 0x1f
-    month = (date >> 5) & 0xf
+    date = dostime >> 16 & 0xFFFF
+    time = dostime & 0xFFFF
+    day = date & 0x1F
+    month = (date >> 5) & 0xF
     year = 1980 + (date >> 9)
-    second = 2 * (time & 0x1f)
-    minute = (time >> 5) & 0x3f
+    second = 2 * (time & 0x1F)
+    minute = (time >> 5) & 0x3F
     hour = time >> 11
     return (year, month, day, hour, minute, second)
--- unrar/cffi/unrarlib.py
+++ unrar/cffi/unrarlib.py
@@ -1,98 +1,112 @@
-from ._unrarlib import ffi
-from ._unrarlib.lib import \
-    RAROpenArchiveEx, \
-    RARCloseArchive, \
-    RARReadHeaderEx, \
-    RARSetCallbackPtr, \
-    RARProcessFileW, \
-    UCM_PROCESSDATA, \
-    PyUNRARCALLBACKStub, \
-    C_RAR_OM_LIST_INCSPLIT, \
-    C_RAR_OM_EXTRACT, \
-    C_RAR_SKIP, \
-    C_RAR_TEST, \
-    C_RAR_EXTRACT, \
-    C_ERAR_SUCCESS, \
-    C_RHDF_DIRECTORY
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Generator
+
+from ._unrarlib import ffi  # type: ignore
+from ._unrarlib.lib import (  # type: ignore
+    C_ERAR_SUCCESS,
+    C_RAR_OM_EXTRACT,
+    C_RAR_OM_LIST_INCSPLIT,
+    C_RAR_SKIP,
+    C_RAR_TEST,
+    C_RHDF_DIRECTORY,
+    UCM_PROCESSDATA,
+    PyUNRARCALLBACKStub,
+    RARCloseArchive,
+    RAROpenArchiveEx,
+    RARProcessFileW,
+    RARReadHeaderEx,
+    RARSetCallbackPtr,
+)
+
+if TYPE_CHECKING:
+    from os import PathLike
 
 FLAGS_RHDF_DIRECTORY = C_RHDF_DIRECTORY
 
-@ffi.def_extern('PyUNRARCALLBACKStub')
-def PyUNRARCALLBACKSkeleton(msg, user_data, p1, p2):    
+
+@ffi.def_extern("PyUNRARCALLBACKStub")
+def PyUNRARCALLBACKSkeleton(msg, user_data, p1, p2):
     callback = ffi.from_handle(user_data)
     return callback(msg, p1, p2)
 
-class RarArchive(object):
 
-    @staticmethod
-    def open_for_metadata(filename):
-        return RarArchive(filename, C_RAR_OM_LIST_INCSPLIT)
-        
-    @staticmethod
-    def open_for_processing(filename):
-        return RarArchive(filename, C_RAR_OM_EXTRACT)
-
-    def __init__(self, filename, mode):
-        self.comment = ''
-        archive = RAROpenArchiveDataEx(filename, mode)        
+class RarArchive:
+    def __init__(self, filename: "PathLike", mode: int) -> None:
+        self.comment = ""
+        archive = RAROpenArchiveDataEx(filename, mode)
         self.handle = RAROpenArchiveEx(archive.value)
         if archive.value.OpenResult != C_ERAR_SUCCESS:
-            raise BadRarFile("Cannot open {}: OpenResult is {}".format(filename, archive.value.OpenResult))
+            raise BadRarFile(
+                "Cannot open {}: OpenResult is {}".format(
+                    filename, archive.value.OpenResult
+                )
+            )
         self.comment = ffi.string(archive.value.CmtBufW)
 
     def __enter__(self):
         return self
 
-    def __exit__(self, type, value, traceback):
-        result = RARCloseArchive(self.handle)        
+    def __exit__(self, type, value, traceback) -> None:
+        result = RARCloseArchive(self.handle)
         assert result == C_ERAR_SUCCESS
 
-    def iterate_headers(self):
+    def iterate_headers(self) -> Generator["RarHeader", Any, None]:
         header_data = RARHeaderDataEx()
-        res = RARReadHeaderEx(self.handle, header_data)    
+        res = RARReadHeaderEx(self.handle, header_data)
         while res == C_ERAR_SUCCESS:
             yield RarHeader(self.handle, header_data)
             header_data = RARHeaderDataEx()
             res = RARReadHeaderEx(self.handle, header_data)
 
+    @staticmethod
+    def open_for_metadata(filename: "PathLike") -> "RarArchive":
+        return RarArchive(filename, C_RAR_OM_LIST_INCSPLIT)
+
+    @staticmethod
+    def open_for_processing(filename: "PathLike") -> "RarArchive":
+        return RarArchive(filename, C_RAR_OM_EXTRACT)
+
+
 def null_callback(*args):
     pass
+
 
 class RarHeader(object):
     def __init__(self, handle, headerDataEx):
         self.handle = handle
         self.headerDataEx = headerDataEx
-    
+
     @property
-    def FileNameW(self):
+    def FileNameW(self) -> str:
         return ffi.string(self.headerDataEx.FileNameW)
-    
+
     @property
     def FileTime(self):
         return self.headerDataEx.FileTime
-    
-    @property
-    def PackSize(self):
-        return self.headerDataEx.PackSize    
 
     @property
-    def PackSizeHigh(self):
+    def PackSize(self) -> int:
+        return self.headerDataEx.PackSize
+
+    @property
+    def PackSizeHigh(self) -> int:
         return self.headerDataEx.PackSizeHigh
 
     @property
-    def UnpSize(self):
+    def UnpSize(self) -> int:
         return self.headerDataEx.UnpSize
 
     @property
-    def UnpSizeHigh(self):
+    def UnpSizeHigh(self) -> int:
         return self.headerDataEx.UnpSizeHigh
 
     @property
-    def UnpVer(self):
+    def UnpVer(self) -> int:
         return self.headerDataEx.UnpVer
 
     @property
-    def FileCRC(self):
+    def FileCRC(self) -> int:
         return self.headerDataEx.FileCRC
 
     @property
@@ -100,7 +114,7 @@ class RarHeader(object):
         return self.headerDataEx.Flags
 
     @property
-    def HostOS(self):
+    def HostOS(self) -> int:
         return self.headerDataEx.HostOS
 
     @property
@@ -110,36 +124,43 @@ class RarHeader(object):
     def skip(self):
         RARProcessFileW(self.handle, C_RAR_SKIP, ffi.NULL, ffi.NULL)
 
-    def test(self, callback = null_callback):
+    def test(self, callback=null_callback):
         def wrapper(msg, p1, p2):
             if msg == UCM_PROCESSDATA:
-                chunk = ffi.buffer(ffi.cast("char *", p1), p2)                
+                chunk = ffi.buffer(ffi.cast("char *", p1), p2)
                 callback(bytes(chunk))
             return 1
+
         user_data = ffi.new_handle(wrapper)
         RARSetCallbackPtr(self.handle, PyUNRARCALLBACKStub, user_data)
         result = RARProcessFileW(self.handle, C_RAR_TEST, ffi.NULL, ffi.NULL)
         RARSetCallbackPtr(self.handle, ffi.NULL, ffi.NULL)
         if result != C_ERAR_SUCCESS:
             raise BadRarFile("Rarfile corrupted: error code is %d" % result)
-        
+
+
 class BadRarFile(Exception):
     pass
 
-class RAROpenArchiveDataEx(object):
-    def __init__(self, filename, mode):
-        COMMENT_MAX_SIZE = 64 * 1024        
-        self.arcNameW = ffi.new("wchar_t[]", filename)
+
+class RAROpenArchiveDataEx:
+    def __init__(self, filename: "PathLike", mode: int):
+        COMMENT_MAX_SIZE = 64 * 1024
+        self.arcNameW = ffi.new("wchar_t[]", str(filename))
         self.cmtBufW = ffi.new("wchar_t[{}]".format(COMMENT_MAX_SIZE))
-        self.value = ffi.new("struct RAROpenArchiveDataEx *", {
-            'ArcNameW': self.arcNameW,
-            'OpenMode': mode,
-            'CmtBufSize': ffi.sizeof("wchar_t") * COMMENT_MAX_SIZE,
-            'CmtBufW': self.cmtBufW
-        })
-    
+        self.value = ffi.new(
+            "struct RAROpenArchiveDataEx *",
+            {
+                "ArcNameW": self.arcNameW,
+                "OpenMode": mode,
+                "CmtBufSize": ffi.sizeof("wchar_t") * COMMENT_MAX_SIZE,
+                "CmtBufW": self.cmtBufW,
+            },
+        )
+
     def value(self):
         return self.value
+
 
 def RARHeaderDataEx():
     return ffi.new("struct RARHeaderDataEx *")
