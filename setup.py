c959a9eca7a103a588a64547d56387355bc71eaa
noaione <noaione0809@gmail.com>
2023-10-30 13:39:46 +0700

    ci: try to fix macOS build

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     use_scm_version=True,
     packages=("unrar.cffi",),
     install_requires=["cffi"],
-    setup_requires=["cffi", "pytest-runner", "wheel", "setuptools_scm"],
+    # setup_requires=["cffi", "pytest-runner", "wheel", "setuptools_scm"],
     tests_require=["pytest"],
     package_dir={"unrar.cffi": "unrar/cffi"},
     package_data={"unrar.cffi": ["*.dll"]},
