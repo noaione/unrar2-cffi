ffa748acecaf1ea40ad7b66e894de1a225bafe3c
noaione <noaione0809@gmail.com>
2023-10-30 13:44:51 +0700

    ci: try to fix setuptools not finding buildconf

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
+# Explicitly use sys to get buildconf.py from the current directory because
+# build_meta backend by pyproject is not finding the file properly.
+
+import sys
 from distutils.command.build import build
 
 from setuptools import setup
 
-from buildconf import BuildUnrarCommand
+sys.path.insert(0, "")
+
+# Import buildconf.py
+from buildconf import BuildUnrarCommand  # noqa: E402
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
