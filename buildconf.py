90fb33392e62b791a2ab7b0abf7f94dbbb25c361
noaione <noaione0809@gmail.com>
2024-03-21 10:15:14 +0700

    More fix to build override

--- buildconf.py
+++ buildconf.py
@@ -132,6 +132,9 @@ class BuildOverride(build):
         self.run_command("build_unrar")
         first_mod = self.distribution.ext_modules[0]
         first_source = first_mod.sources[0]
+        dist_parent = create_builder().distutils_extension()
+        log.info("data: %s", dist_parent.sources)
+
         if first_source == "$PLACEHOLDER":
             self.distribution.ext_modules[0].sources[0] = "build/unrar/cffi/_unrarlib.c"
         build.run(self)
